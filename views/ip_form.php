08cee13ebbb1b6fd1018fd1aaa85cb85a9f492dc
Anik2069 <anikislam668@gmail.com>
2022-08-28 01:39:34 +0600

    title

--- views/ip_form.php
+++ views/ip_form.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="shortcut icon" href="favicon.png">
-    <title>My Application</title>
+    <title>IP FormC</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body {
@@ -124,7 +124,7 @@
 </head>
 
 <body>
-    <h4 class="text-center">Welcome To Question Answer Session</h4> <br>
+    <h4 class="text-center">IP Checker</h4> <br>
     <div class="container">
         <div class="row">
             <div class="col-lg-2">
@@ -136,26 +136,14 @@
                             <div class="card">
                                 <div class="card-header">
 
-                                    <h5>
-                                        <a href="<?php echo $routeToSolution1 ?>" class="btn btn-link" type="button">Q1 . We have a set of data in the following url: http://103.219.147.17/api/json.php
-                                            From this data we need to find the speeds those have crossed 60. The result should contain the list &
-                                            the total number of speeds those have crossed 60.</a>
-                                    </h5>
-
-                                    <h5>
-                                        <a href="<?php echo $routeToSolution2 ?>" class="btn btn-link" type="button">Q2 .We have an array like
-                                            array('0'=>'z1', '1'=>'Z10', '2'=>'z12', '3'=>'Z2', '4'=>'z3')
-                                            We need to sort this. You can use this given array for your sorted solution.
-                                            Expected Output:
-                                            array('0'=>'z1', '3'=>'Z2', '4'=>'z3', '1'=>'Z10', '2'=>'z12')</a>
-                                    </h5>
-
-
-                                    <h5>
-                                        <a href="<?php echo $routeToSolution3 ?>" class="btn btn-link" type="button">Q3 .We need to validate any IP address.
-                                            We are sure that you can write a logic to validate any IP address and return true if valid and false if
-                                            invalid. We hope to see that you do this without any built-in function.</a>
-                                    </h5>
+                                    <form action="../submit-ip" method="post">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">IP Address</label>
+                                            <input type="text" name="ip" class="form-control">
+                                        </div>
+                                    
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </form>
                                 </div>
 
                             </div>
